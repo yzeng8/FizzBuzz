b4f1c33ea27754e4c45d40298bbd5476a6829a73
yzeng8 <yzeng8@ualberta.ca>
2024-09-08 16:31:15 -0400

    access modifier added

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+//        FizzBuzz.doFizzBuzz(1);
+//        FizzBuzz.doFizzBuzz(2);
+//        FizzBuzz.doFizzBuzz(3);
+//        FizzBuzz.doFizzBuzz(4);
+//        FizzBuzz.doFizzBuzz(5);
+
+        FizzBuzz.main(args);
+
+
+    }
+}
